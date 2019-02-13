b27565030f11b3fea8a454018b56bce352828ace
Duilio Palacios <duilio@styde.net>
2019-02-13 15:49:27 +0000

    Start with procedural code instead

--- template/public/image-jpg.php
+++ template/public/image-jpg.php
@@ -2,9 +2,6 @@
 
 require_once __DIR__.'/../bootstrap/app.php';
 
-use Styde\ImageJpeg;
-
-$image = new ImageJpeg(assets_path('img/tower-bridge.jpg'));
-
+$img = imagecreatefromjpeg(assets_path('img/tower-bridge.jpg'));
 header('Content-Type: image/jpeg');
-imagejpeg($image->draw());
+imagejpeg($img);
--- template/public/image-png.php
+++ template/public/image-png.php
@@ -2,9 +2,6 @@
 
 require_once __DIR__.'/../bootstrap/app.php';
 
-use Styde\ImagePng;
-
-$image = new ImagePng(assets_path('img/tower-bridge.png'));
-
+$img = imagecreatefrompng(assets_path('img/tower-bridge.png'));
 header('Content-Type: image/png');
-imagepng($image->draw());
+imagepng($img);
